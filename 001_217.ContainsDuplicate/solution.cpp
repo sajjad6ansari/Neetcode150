0b35ec1929c4523910b85dd76c81a6082ceb4dc4
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-05-23 15:14:51 +0530

    solution.cpp
    Explanation:
    unordered_set is used because it offers average O(1) time complexity for find() and insert() operations.
    
    We loop through the vector:
    
    If v[i] is already present in the set (i.e., duplicate), we return true.
    
    Otherwise, insert it into the set.
    
    If the loop finishes without finding duplicates, return false.
    
    This approach ensures high efficiency in both time and space.

--- 001_217.ContainsDuplicate/solution.cpp
+++ 001_217.ContainsDuplicate/solution.cpp
@@ -8,12 +8,13 @@ public:
     bool containsDuplicate(vector<int>& v) {
         // Create an unordered_set to store unique elements
         unordered_set<int> s;
-
+        
         // Get the size of the vector
         int n = v.size();
 
         // Iterate through each element in the vector
         for (int i = 0; i < n; i++) {
+            
             // Check if the current element already exists in the set
             if (s.find(v[i]) != s.end()) {
                 return true; // Duplicate found, return true
