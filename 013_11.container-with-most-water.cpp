265c6b75b7f53dcae5beaf33ef3b8dab75383a40
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-07-01 22:01:52 +0530

    MEDIUM

--- 013_11.container-with-most-water.cpp
+++ 013_11.container-with-most-water.cpp
@@ -34,30 +34,25 @@ public:
 
 //Approach-2 (Two pointer Greedy (O(N)) (It's similar to Approach-1)
 //Just that we are eliminating heights at one go
+// Approach-2: Optimized Two Pointer (O(N)) 
+// Skip all bars smaller than current height after considering the area
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        int n = height.size();
-        int i = 0, j = n-1;
-        
+        int i = 0, j = height.size() - 1;
         int water = 0;
-        while(i<j) {
-            
-            //start from the smallest one and calculate water
+        
+        while (i < j) {
             int h = min(height[i], height[j]);
-            int w = j-i;
-            int area = h*w;
-            water = max(water, area);
+            int w = j - i;
+            water = max(water, h * w);
             
-            //Then move towards large one because we can have better answer
-            if(height[i] <
-            while(i < j && height[i] <= h)
-                i++;
-            //Same,  move towards large one because we can have better answer
-            while(i < j && height[j] <= h)
-                j--;
+            // Move both pointers inward while skipping equal/smaller heights
+            while (i < j && height[i] <= h) i++;
+            while (i < j && height[j] <= h) j--;
         }
         
         return water;
     }
 };
+
